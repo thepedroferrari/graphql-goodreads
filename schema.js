ffd8f45dcf6493b6a30a4b4138e26f94113f0ce3
Pedro Ferrari <pedro@pedroferrari.com>
2018-08-04 16:56:43 +0200

    Working with the module exports

--- schema.js
+++ schema.js
@@ -1,8 +1,33 @@
-import { GraphQLSchema } from 'graphql';
 const fetch = require('node-fetch');
 const util = require('util');
 const parseXML = util.promisify(require('xml2js').parseString);
+const { GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString } = require('graphql');
 
-fetch('https://www.goodreads.com/author/show.xml?id=4432&key=UFScpw1SBAOKEQ5vaBbOjg')
-	.then((response) => response.text())
-	.then(parseXML);
+const AuthorType = new GraphQLObjectType({
+	name: 'Author',
+	description: '...',
+	fields: () => ({
+		name: {
+			type: GraphQLString
+		}
+	})
+});
+
+module.exports = new GraphQLSchema({
+	query: new GraphQLObjectType({
+		name: 'Query',
+		description: '...',
+		fields: () => ({
+			author: {
+				type: AuthorType,
+				args: {
+					id: { type: GraphQLInt }
+				},
+				resolve: (root, args) =>
+					fetch('https://www.goodreads.com/author/show.xml?id=${args.id}&key=UFScpw1SBAOKEQ5vaBbOjg')
+						.then((response) => response.text())
+						.then(parseXML)
+			}
+		})
+	})
+});
